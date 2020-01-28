069dc2a6cd5f2624f0a7f03df9a53c2c116b016f
Kuba <Kuba@mail.com>
2020-01-28 13:04:28 +0100

    Initial pure React Pet component

---
+++ src/App.js
@@ -0,0 +1,30 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", { id: "something-important" }, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Bira",
+      animal: "Dog",
+      breed: "German Shepherd"
+    }),
+    React.createElement(Pet, {
+      name: "Remi",
+      animal: "Dog",
+      breed: "German Shepherd"
+    }),
+    React.createElement(Pet, {
+      name: "Roman",
+      animal: "Cat",
+      breed: "Roof walker"
+    })
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
